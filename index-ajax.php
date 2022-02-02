358957f8e1b4ff17cc3b10dd9ab4a0aa4afaf566
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-02 17:42:30 +0100

    add axios cards ajax

---
+++ index-ajax.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <title>Ajax</title>
+</head>
+<body>
+    <?php include_once __DIR__ . '/partials/header.php' ?>
+    <main class="main-cont" id="app">
+            <div class="cards" v-for="card in cards">
+                <img class="img" v-bind:src="'img/' + card.img" /> 
+                    {{card.title}}
+            </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+    <script src="js/script.js"></script>
+    
+</body>
+</html>
